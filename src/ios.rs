28eeecd93029db4b756844852e3608eba83bf455
Jesse Luehrs <doy@tozt.net>
2013-03-20 00:09:10 -0500

    rearrange things a bit

--- src/ios.rs
+++ src/ios.rs
@@ -1,5 +1,30 @@
 use core::libc::c_int;
 
+pub fn cooked () -> int {
+    unsafe { c::cooked() as int }
+}
+
+pub fn cbreak () -> int {
+    unsafe { c::cbreak() as int }
+}
+
+pub fn raw () -> int {
+    unsafe { c::raw() as int }
+}
+
+pub fn echo (enable: bool) -> int {
+    unsafe { c::echo(enable as c_int) as int }
+}
+
+pub fn preserve<T> (body: &fn () -> T) -> T {
+    let _guard = PreserveTermios();
+    body()
+}
+
+pub fn isatty() -> bool {
+    unsafe { c_isatty(0) as bool }
+}
+
 enum struct_termios {}
 
 struct PreserveTermios {
@@ -30,29 +55,4 @@ extern mod c {
 extern {
     #[link_name = "isatty"]
     fn c_isatty(fd: c_int) -> c_int;
-}
-
-pub fn cooked () -> int {
-    unsafe { c::cooked() as int }
-}
-
-pub fn cbreak () -> int {
-    unsafe { c::cbreak() as int }
-}
-
-pub fn raw () -> int {
-    unsafe { c::raw() as int }
-}
-
-pub fn echo (enable: bool) -> int {
-    unsafe { c::echo(enable as c_int) as int }
-}
-
-pub fn preserve<T> (body: &fn () -> T) -> T {
-    let _guard = PreserveTermios();
-    body()
-}
-
-pub fn isatty() -> bool {
-    unsafe { c_isatty(0) as bool }
 }
