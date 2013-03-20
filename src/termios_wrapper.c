65c1594991f029ed30669885a9716c1f282be6b6
Jesse Luehrs <doy@tozt.net>
2013-03-19 22:13:08 -0500

    implement echo

--- src/termios_wrapper.c
+++ src/termios_wrapper.c
@@ -14,6 +14,20 @@ int raw()
 
 int echo(int enabled)
 {
+    struct termios t;
+
+    if (tcgetattr(0, &t) == -1) {
+        return 0;
+    }
+
+    if (enabled) {
+        t.c_lflag |= ECHO;
+    }
+    else {
+        t.c_lflag &= ~ECHO;
+    }
+
+    return tcsetattr(0, TCSANOW, &t) == 0;
 }
 
 int crlf(int enabled)
