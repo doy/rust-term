771c8bebde83ca8f8395078c44e582e7c25200d0
Jesse Luehrs <doy@tozt.net>
2013-03-19 22:48:25 -0500

    more implementations

--- src/termios_wrapper.c
+++ src/termios_wrapper.c
@@ -1,15 +1,49 @@
 #include <termios.h>
 
+/* very simplistic, ignores a lot of the settings that i don't understand,
+ * patches welcome */
+
 int cooked()
 {
+    struct termios t;
+
+    if (tcgetattr(0, &t) == -1) {
+        return 0;
+    }
+
+    t.c_lflag |= (ICANON | ISIG | IEXTEN);
+    t.c_iflag |= (IXON | BRKINT);
+
+    return tcsetattr(0, TCSANOW, &t) == 0;
 }
 
 int cbreak()
 {
+    struct termios t;
+
+    if (tcgetattr(0, &t) == -1) {
+        return 0;
+    }
+
+    t.c_lflag |= ISIG;
+    t.c_lflag &= ~(ICANON | IEXTEN);
+    t.c_iflag |= (IXON | BRKINT);
+
+    return tcsetattr(0, TCSANOW, &t) == 0;
 }
 
 int raw()
 {
+    struct termios t;
+
+    if (tcgetattr(0, &t) == -1) {
+        return 0;
+    }
+
+    t.c_lflag &= ~(ICANON | ISIG | IEXTEN);
+    t.c_iflag &= ~(IXON | BRKINT);
+
+    return tcsetattr(0, TCSANOW, &t) == 0;
 }
 
 int echo(int enabled)
