de4fa4fcd6cf70dd13a5b71ef9f62f4c417ce774
Dave King <dave@davbo.org>
2014-01-16 22:55:13 +0000

    Fixed tput test

--- test/tput.rs
+++ test/tput.rs
@@ -1,4 +1,5 @@
 extern mod term;
+use std::os;
 
 fn main () {
     if os::args().len() < 2 {
@@ -8,7 +9,7 @@ fn main () {
     term::info::init();
     let attr: &str = os::args()[1];
     let escape = term::info::escape(attr).expect(
-        fmt!("%s is not supported on this terminal", attr)
+        format!("{:s} is not supported on this terminal", attr)
     );
     print(escape);
 }
